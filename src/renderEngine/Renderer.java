ffa3834442b6f22df6dc06f3168cb7bcd96677ed
BramDaily <liam.james.brady@gmail.com>
2018-09-01 15:45:49 -0400

    adding a obj loader

--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -31,7 +31,7 @@ public class Renderer {
 	public void prepare() {
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		GL11.glClearColor(0.5f, 0, 0.4f, 1);
+		GL11.glClearColor(0.1f, 0.1f, 0.1f, 1);
 	}
 	
 	public void render(Entity entity, StaticShader shader) {
