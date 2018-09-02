907ec22cc29d676d83c3b79f0b9bfad37fdea18c
BramDaily <liam.james.brady@gmail.com>
2018-09-02 19:00:10 -0400

    added player and third person camera

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import org.lwjgl.util.vector.Vector3f;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
+import entities.Player;
 import models.RawModel;
 import models.TexturedModel;
 import objConverter.ModelData;
@@ -18,6 +19,8 @@ import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import terrains.Terrain;
 import textures.ModelTexture;
+import textures.TerrainTexture;
+import textures.TerrainTexturePack;
 
 public class MainGameLoop {
 	 
@@ -32,30 +35,41 @@ public class MainGameLoop {
         //////////////////////////////////////////////////////////////////////////////////////////
         //Loading Models
         //////////////////////////////////////////////////////////////////////////////////////////
-        ModelData ringData = OBJFileLoader.loadOBJ("ring");
-        RawModel ringModel = loader.loadToVAO(ringData.getVertices(), ringData.getTextureCoords(), ringData.getNormals(), ringData.getIndices());
-        ModelTexture ringTexture = new ModelTexture(loader.loadTexture("ringTexture"));
-        Entity ring = new Entity(new TexturedModel(ringModel, ringTexture), new Vector3f(0, 10, -20), 0, 0, 0, 1);
+        ModelData personData = OBJFileLoader.loadOBJ("person");
+        RawModel personModel = loader.loadToVAO(personData.getVertices(), personData.getTextureCoords(), personData.getNormals(), personData.getIndices());
+        ModelTexture personTexture = new ModelTexture(loader.loadTexture("textures/playerTexture"));
+        Player person = new Player(new TexturedModel(personModel, personTexture), new Vector3f(0, 10, -20), 0, 0, 0, 1);
         
         
         
-        entities.add(ring);
+        entities.add(person);
+        //////////////////////////////////////////////////////////////////////////////////////////
+        //Terrain Texture
         //////////////////////////////////////////////////////////////////////////////////////////
         
-        Terrain terrain1 = new Terrain(0, -1,loader,new ModelTexture(loader.loadTexture("grass")));
-        Terrain terrain2 = new Terrain(-1, -1,loader,new ModelTexture(loader.loadTexture("grass")));
+        TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("textures/grassy"));
+        TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("textures/dirt"));
+        TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("textures/pinkFlowers"));
+        TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("textures/path"));
+        
+        TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
+        TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMaps/blendMap"));
+        
+        //////////////////////////////////////////////////////////////////////////////////////////
+        
+        Terrain terrain1 = new Terrain(0, -1,loader, texturePack, blendMap);
+        Terrain terrain2 = new Terrain(-1, -1,loader, texturePack, blendMap);
         terrains.add(terrain1); terrains.add(terrain2);
          
          
         Light light = new Light(new Vector3f(20000,20000,2000),new Vector3f(1,1,1));
          
-        Camera camera = new Camera();   
+        Camera camera = new Camera(person);   
         MasterRenderer renderer = new MasterRenderer();
          
         while(!Display.isCloseRequested()){
-        	ring.increaseRotation(10, 10, 10);
             camera.move();
-             
+            person.move();
             for (Terrain terrain : terrains)
             	renderer.processTerrain(terrain);
             for (Entity entity : entities)
